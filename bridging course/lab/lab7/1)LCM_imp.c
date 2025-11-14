5d82ff26277faf3c9469e42d8f3d63b7d7022c8a
lycrain <lycrain200808@gmail.com>
2025-11-14 17:05:17 +0800

    lab7. Enhance LCM function to manage zero and negative values
    Update LCM calculation to correctly handle negative inputs.

--- bridging course/lab/lab7/1)LCM_imp.c
+++ bridging course/lab/lab7/1)LCM_imp.c
@@ -5,15 +5,15 @@ int main(){
 	{
 		printf ("Enter: ");
 		scanf("%d%d",&S,&L);
-		if (S<=0) break;
+		if (S<=0 || L<=0) break;
 		else {
 				CM=L;
-				while(!(CM%S==0&&CM%L==0))
+				while(!(CM%S==0))
 				{
 					CM+=L;
 				}
 				printf("%d\n",CM);
 			}
-} while (S>=0);
+} while (S>0 && L>0);
 	return 0;
 }
